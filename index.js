78c37886e9205b9e0a5c92a2fbfba4fcf1085ead
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-10 12:53:19 -0400

    commit tree 16d0a7b07f5783a2f82d25a6c45dc4f38e725358

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
